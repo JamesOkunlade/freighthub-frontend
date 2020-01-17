b8aaaee0f8ae8da4d5fc0af6bfac9f7534f2a1cf
James Okunlade <james.okunlade@gmail.com>
2020-01-17 10:44:19 +0100

    bootstrap added and homepage created

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Switch } from "react-router";
 
 import Home from "../pages/Home";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import {Helmet} from "react-helmet";
+import {Link} from "react-router-dom";
+
+import "../styles/Home.css";
 
 
 
 
 class Home extends Component {
   render () {
-    return(  
+    return(
       <div>
         <Helmet>
           <meta name="description" content="Frieghthub Homepage"/>
           <title>Frieghthub - Home </title>
         </Helmet>
-        <div>This is Home page</div>
+        <div className="home">
+          <div className="cap">
+            <h1>Frieghthub</h1>
+            <Link to="./shipments"><button type="button" class="btn btn-outline-secondary">View all shipments</button></Link>
+          </div>
+        </div>
       </div>
     );
   }
